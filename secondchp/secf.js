3d3cdf4f7a3afc70b6607059a049180605088150
smriti <rajsimmi369@gmail.com>
2024-01-01 21:57:05 +0530

    About Operators

---
+++ secondchp/secf.js
@@ -0,0 +1,55 @@
+ //Artimetic Operators
+
+
+ let a = 6;
+ let b = 2;
+
+  console.log("a=",a, "& b=", b);
+  console.log("a + b =", a+b);
+  console.log("a - b =", a-b);
+ console.log("a * b =", a*b);
+  console.log("a / b =", a/b);
+console.log("a % b =", a%b);
+  console.log("a ** b =", a**b);
+
+ //unary Operator
+ let d= 8;
+ let c = 2;
+
+
+ console.log("d=",d, "& c =" ,c);
+ d++;
+ console.log("d=",d);
+
+ c--;
+ console.log("c = ",c);
+
+
+ //Assignment Operators
+  let f =4;
+  let g =3;
+
+  f += 4  ;
+  console.log("f =",f);
+  /* * we can use all ariemetic operators
+  in assig opp**/
+
+
+  //Comparision opt
+
+//   let r = 6;
+//   let t = 3;
+//    console.log("6==6",r==r);
+
+   //Logicsl opt
+
+   let r = 6;
+   let t = 3;
+
+   let cond1 = r < t;
+   let cond2 = r ==6;
+
+
+
+   console.log("cond1 && cond1=", cond1 && cond2);
+
