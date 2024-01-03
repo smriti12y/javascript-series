74760984b369ebd5e240abc1f2e926302713f42a
smriti <rajsimmi369@gmail.com>
2024-01-03 23:33:59 +0530

    Intro to DOM

---
+++ Javadoc/script.js
@@ -0,0 +1,18 @@
+//manipualtion of DOM
+/**selcecting by id */
+ let heading=document.getElementById("heading");
+
+ console.dir(heading);
+
+ //selcting by class
+
+ let headss= document.getElementsByClassName("heads");
+
+ console.log(headss);
+
+
+ //selcting by Tags
+
+ let paras= document.getElementsByTagName("p");
+
+ console.log(paras);
