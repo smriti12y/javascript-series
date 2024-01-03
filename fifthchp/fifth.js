31651f981af3c5533ee83e05c55cbafdc295d3c3
smriti <rajsimmi369@gmail.com>
2024-01-03 19:55:08 +0530

    Arrow func

--- fifthchp/fifth.js
+++ fifthchp/fifth.js
@@ -7,5 +7,17 @@ myFunction();
 myFunction();
 myFunction();
 
+//Arrow function
 
+//modern js 
+const arrowsum = (a,b)=> {
+    console.log(a+b)
+};
+
+
+// multiplication function
+const arrowMul = (c,d)=>{
+    console.log(c * d)
+};
+ 
   
