d80e737b00213afdcc8fd3eb59a7c2cf477ac8cb
smriti <rajsimmi369@gmail.com>
2024-01-07 19:08:36 +0530

    i created scoreBoard

--- chp9/script.js
+++ chp9/script.js
@@ -8,13 +8,40 @@ const genCompChoice=()=>{
     const randIdx= Math.floor(Math.random()*3);
     return options[randIdx];
 
-}
+};
+const drawGame= ()=>{
+    console.log("game was draw.");
+};
+
+const showWinner=(userWin)=>{
+    if(userWin){
+        console.log("you win!");
+    } else {
+        console.log("you lose");
+    }
+};
 
 const playGame=(userChoice)=>{
-    console.log("userChoice=",userChoice);
+    console.log("user Choice=",userChoice);
     //Generate computer choice-->
     const compChoice=genCompChoice();
     console.log("comp Choice=",compChoice );
+    if(userChoice === compChoice){
+        //Draw Game
+        drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice  ==="rock"){
+            userWin = compChoice ==="paper" ? false:true;
+        } else if (userChoice ==="paper")
+        {
+            userWin=compChoice="scissors"? false:true;
+        } else{
+            userWin= compChoice==="rock"?false:true;
+       }
+       showWinner(userWin);
+
+    }
 };
 
 
@@ -26,3 +53,4 @@ choices.forEach((choice)=>{
         playGame(userChoice);
     });
 });
+
