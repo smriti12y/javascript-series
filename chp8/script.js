0ba5bf5e92ea336af00975e9ece328f6c960211f
smriti <rajsimmi369@gmail.com>
2024-01-07 15:01:01 +0530

    Event listener

--- chp8/script.js
+++ chp8/script.js
@@ -7,12 +7,24 @@ let btn = document.querySelector("#btn");
 
 //Event Listenser
   btn.addEventListener("click", ()=>{
-    console.log("button was clicked");
-    console.log(evt);
-    console.log(evt.type);
-    
-
+    console.log("button was clicked -handler1");
   });
+  btn.addEventListener("click", ()=>{
+    console.log("button was clicked -handler2");
+  });
+
+  // for removing an event 
+
+  let handler3=()=>{ console.log("button was clicked -handler3");
+};
+
+  btn.addEventListener("click",handler3);
+  
+  btn.addEventListener("click", ()=>{
+    console.log("button was clicked -handler4");
+ });
+9
+btn.removeEventListener("click",handler3);
 
 
 let div = document.querySelector("div");
