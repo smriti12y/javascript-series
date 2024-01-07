fd26d450423a23c025bee5beb550c63726329958
smriti <rajsimmi369@gmail.com>
2024-01-07 18:27:50 +0530

    generated comp and user choice

--- chp9/script.js
+++ chp9/script.js
@@ -3,10 +3,26 @@ let compScore=0;
 
 const choices=document.querySelectorAll(".choice");
 
+const genCompChoice=()=>{
+    const options = ["rock","paper","scissors"];
+    const randIdx= Math.floor(Math.random()*3);
+    return options[randIdx];
+
+}
+
+const playGame=(userChoice)=>{
+    console.log("userChoice=",userChoice);
+    //Generate computer choice-->
+    const compChoice=genCompChoice();
+    console.log("comp Choice=",compChoice );
+};
+
+
 choices.forEach((choice)=>{
     choice.addEventListener("click",()=>
     {
         const  userChoice=choice.getAttribute("id");
-        console.log("choice was clicked",userChoice);
-    })
-})
+       
+        playGame(userChoice);
+    });
+});
