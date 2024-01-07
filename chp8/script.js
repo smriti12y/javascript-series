88915555918ed9f30b795b44cdf625b69f200fbd
smriti <rajsimmi369@gmail.com>
2024-01-07 14:53:23 +0530

    event properties

--- chp8/script.js
+++ chp8/script.js
@@ -1,11 +1,25 @@
 let btn = document.querySelector("#btn");
 
-btn.onclick= ()=>{
-    console.log("btn was clicked");
-}
+//  btn.onclick= ()=>{
+//      console.log("btn was clicked");
+    
+//     }
+
+//Event Listenser
+  btn.addEventListener("click", ()=>{
+    console.log("button was clicked");
+    console.log(evt);
+    console.log(evt.type);
+    
+
+  });
+
 
 let div = document.querySelector("div");
 
-div.onmouseover=()=>{
+div.onmouseover=()=>{ 
     console.log("welome my freind welcome!");
 }
+
+//Event object
+
