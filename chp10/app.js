5f033d44c2e000d48fd23c0ce97eaafe51090e57
smriti <rajsimmi369@gmail.com>
2024-01-08 23:03:17 +0530

    implementation of proto

--- chp10/app.js
+++ chp10/app.js
@@ -1,7 +1,41 @@
 const student={
-    fullName:"smriti pandey",
-    marks:94.4,
-    printMarks: function(){
-        console.log("marks=",this. marks);
+     fullName:"smriti pandey",
+     marks:94.4,
+     printMarks: function(){
+         console.log("marks=",this. marks);
+     },
+    }
+    ;
+ 
+const employee={
+    calcTax(){
+        console.log("tax rate is 10%");
+
     },
 };
+
+const SmritiSimmi={
+    salary:500000,
+};
+const SmritiSimmi2={
+    salary:500000,
+};
+const SmritiSimmi3={
+    salary:500000,
+};
+const SmritiSimmi4={
+    salary:500000,
+};
+const SmritiSimmi5={
+    salary:500000,
+};
+const SmritiSimmi6={
+    salary:500000,
+};
+
+SmritiSimmi.__proto__= employee;
+SmritiSimmi2.__proto__= employee;
+SmritiSimmi3.__proto__= employee;
+SmritiSimmi4.__proto__= employee;
+SmritiSimmi5.__proto__= employee;
+SmritiSimmi6.__proto__= employee;
