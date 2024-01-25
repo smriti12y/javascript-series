bd044ddc36676f176ddae764443715efae1c52d6
smriti <rajsimmi369@gmail.com>
2024-01-25 22:46:28 +0530

    basic practicing

--- basic/script.js
+++ basic/script.js
@@ -1,32 +1,57 @@
-let  fruitsName=["mango","pears"];
-console.log(fruitsName);
+
 
 
 let marks =[78,65,34,23,34];
 console.log(marks);
 
 
-let studentsArray = [
-    { name: 'John', age: 20, marks: 85 },
-    { name: 'Alice', age: 22, marks: 92 },
-    { name: 'Bob', age: 21, marks: 78 },
-    { name: 'Eve', age: 23, marks: 95 },
+// let studentsArray = [
+//     { name: 'John', age: 20, marks: 85 },
+//     { name: 'Alice', age: 22, marks: 92 },
+//     { name: 'Bob', age: 21, marks: 78 },
+//     { name: 'Eve', age: 23, marks: 95 },
    
-  ];
+//   ];
   
 
-  studentsArray.forEach((student, index) => {
-    console.log(`Student ${index + 1}:`);
-    console.log(`Name: ${student.name}`);
-    console.log(`Age: ${student.age}`);
-    console.log(`Marks: ${student.marks}`);
+//   studentsArray.forEach((student, index) => {
+//     console.log(`Student ${index + 1}:`);
+//     console.log(`Name: ${student.name}`);
+//     console.log(`Age: ${student.age}`);
+//     console.log(`Marks: ${student.marks}`);
     
-  });
+//   });
 
-  function multiplyNumbers(a, b) {
-    let sum = a * b;
-    return sum;  // The result of the addition operation is returned
-  }
+//   function multiplyNumbers(a, b) {
+//     let sum = a * b;
+//     return sum;  // The result of the addition operation is returned
+//   }
   
-  let result = multiplyNumbers(5, 7);
-  console.log(result);
+  // let result = multiplyNumbers(5, 7);
+  // console.log(result);
+
+  let person = {
+    name: 'smriti',
+    age: 25,
+    isStudent: true
+  };
+  console.log(person.name);
+
+  const student5 = {
+    fullName: "smriti",
+      age: 21,
+      isValid:true,
+        sports:null,
+        getScore: function(){
+          return 95;
+        },
+    getFriend: ["shanu","bitu"],
+      getSubjects: {
+        subject1:"science", //create a nested object fro subj which includes marks subject names 
+        subject2:"maths"
+      }
+  
+  };
+  //india profile usinh every dta types 
+  // update very property of object of every of property
+// create profile od 5students in array using for loop
